a2b51d0da0a82de80b1c4a816df50a27973d2e48
Jonah Witcig <jwitcig@gmail.com>
2017-01-08 15:38:46 -0600

    Updated calls.

--- builder/views.py
+++ builder/views.py
@@ -10,10 +10,9 @@ def fly(request):
     call(['rm', '-rf', 'durputtbuilder/operation-mr-putt-putt'])
 
 
-    process = subprocess.Popen(['git', 'clone', '-b', 'dev', 'https://github.com/jwitcig/operation-mr-putt-putt'])
-    process.wait()
+    call(['git', 'clone', '-b', 'dev', 'https://github.com/jwitcig/operation-mr-putt-putt'])
+    
 
-    process = subprocess.Popen(['fastlane', 'ios', 'fly'], cwd='operation-mr-putt-putt')
-    process.wait()
+    call(['fastlane', 'ios', 'fly'], cwd='operation-mr-putt-putt')
 
     return HttpResponse()
